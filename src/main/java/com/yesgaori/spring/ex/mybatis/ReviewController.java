b8b133a8ba74e2d08ffa092095c65b1234c60934
khy20100065@gmail.com <khy20100065@gmail.com>
2023-08-25 18:49:06 +0900

    [강하영]  - mybatis insert 및 update 진행

--- src/main/java/com/yesgaori/spring/ex/mybatis/ReviewController.java
+++ src/main/java/com/yesgaori/spring/ex/mybatis/ReviewController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yesgaori.spring.ex.mybatis.domain.Review;
 import com.yesgaori.spring.ex.mybatis.service.ReviewService;
-
+@RequestMapping("/mybatis/review")
 @Controller
 public class ReviewController {
 	
@@ -16,13 +16,37 @@ public class ReviewController {
 	private ReviewService reviewService;
 	
 	// 리퀘스트 파라미터와 전달된 id와 일치하는 리뷰 정보를 json으로 response에 담는다
-	@RequestMapping("/mybatis/review")
+	@RequestMapping("/id")
 	@ResponseBody
 	// request.getParameter("id");
 	public Review review(@RequestParam("id") int id) {
 		Review review = reviewService.getReview(id);
 		
 		return review;
+	}
+	
+	@RequestMapping("/insert")
+	@ResponseBody
+	public String createReview() {
+		
+		// 4, 치즈피자, 김인규, 4.5
+//		int count = reviewService.addReview(4, "치즈피자", "김인규", 4.5);
+		
+		// 2, 뿌링클, 김인규, 4.0, 역시 뿌링클은 진리 입니다.
+		Review review = new Review();
+		review.setStoreId(2);
+		review.setMenu("뿌링클");
+		review.setUserName("김인규");
+		review.setPoint(4.0);
+		review.setReview("역시 뿌링클은 진리 입니다.");
+		
+		int count = reviewService.addReviewByObject(review);
+		
+		return "수행결과" + count;
+		
+		
+		
+		
 	}
 	
 }
--- src/main/java/com/yesgaori/spring/ex/mybatis/repository/ReviewRepository.java
+++ src/main/java/com/yesgaori/spring/ex/mybatis/repository/ReviewRepository.java
@@ -11,4 +11,14 @@ public interface ReviewRepository {
 	// 파라미터로 전달된 id와 일치하는 한행 리뷰 데이터 조회
 	public Review selectReview(@Param("id") int id);
 	
+	// 전달받은 리뷰내용을 insert 하는 기능
+	public int insertReview(
+			@Param("storeId")int storeId
+			, @Param("menu")String menu
+			, @Param("userName")String userName
+			, @Param("point")double point);
+	
+	// 전달받은 리뷰 객체 내용을 insert하는 기능
+	public int insertReviewByObject(Review review);
+	
 }
